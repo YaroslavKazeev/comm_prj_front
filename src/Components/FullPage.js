df7e00f0f1d3beb625317247558c56970e64bde4
Heorhii <jorik142@gmail.com>
2023-07-04 21:59:04 +0200

    fullpage for post in relation with id

--- src/Components/FullPage.js
+++ src/Components/FullPage.js
@@ -1,16 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import { useParams, useNavigate, Link } from "react-router-dom";
+import Header from "./Header";
+import '../styles/fullPage.css'
 
 
 const FullPage = () => {
+    const [title, setTitle]= useState([]);
+    const [desc, setDesc] = useState([])
+    const [owner, setOwner]= useState([])
+    const [time, setTime] = useState([])
 
     let {id} = useParams();
 
     useEffect(() =>{
         axios.get(`http://localhost:5000/fullPage/${id}`)
             .then(result =>{
-                console.log(result)
+                let res= result.data.posts
+                setTitle(res.title)
+                setDesc(res.desc)
+                setTime(res.creat_at)
 
             })
             .catch(err =>{
@@ -18,7 +27,22 @@ const FullPage = () => {
             })
     },[id])
     return(
-        <h3>Hello</h3>
+        <>
+        <Header />
+        <section className={'post_section'}>
+            <div className="container">
+                <div className="post_wrapper">
+                    <h3>Title :{title}</h3>
+                    <hr/>
+                    <p>Desc : {desc}</p>
+                    <p>Time: {time}</p>
+
+
+
+                </div>
+            </div>
+        </section>
+        </>
     )
 }
 export default FullPage;

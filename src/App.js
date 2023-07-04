99eba35e86bb21bd39d6337e38611ac8354e6d1a
YaroslavKazeev <yaroslav.kazeyev@gmail.com>
2023-07-04 11:32:41 +0200

    the Router in App, startPage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import editPage from './components/editPage';
+import fullPage from './components/fullPage';
+import logInPage from './components/logInPage';
+import questionPage from './components/questionPage';
+import signupPage from './components/signupPage';
+import startPage from './components/startPage';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/edit_page/:id" element={<editPage />} />
+        <Route path="/fullPage/:id" element={<fullPage />} />
+        <Route path="/login" element={<logInPage />} />
+        <Route path="/questionPage" element={<questionPage />} />
+        <Route path="/signup" element={<signupPage />} />
+        <Route path="/" element={<startPage />} />
+      </Routes>
     </div>
   );
 }
